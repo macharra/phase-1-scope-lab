4bbb1c338681a00285374872289cadd60619e4a7
rogers.machara <rogersmachara13@gmail.com>
2024-03-22 11:51:25 +0300

    determines scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomer(customerName) {
+    customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+
+}
+
+function overWriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    
+}
+
+const leastFavoriteCustomer = 'jon';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'jay'
+}
+
